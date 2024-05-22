4b4fdad35c62fa33764526ff86ba3fab3fca0551
barlog <barlog7@yandex.ru>
2024-05-22 17:16:19 +0300

    add 6 lesson

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,8 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 
 /*public class HelloWorld {
     public static void main(String[] args) {
@@ -25,9 +27,33 @@ public class HelloWorld {
         app.get("/hello", ctx -> {
             //var page = ctx.queryParam("name");
             var page = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
-            ctx.result("Hello, "+ page + "!");
+            ctx.result("Hello, " + page + "!");
+
         });
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            ctx.result("user ID: " + ctx.pathParam("id"));
+            ctx.result("post ID: " + ctx.pathParam("postId"));
+        });
+       /* app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
+
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("courseId"));
+            ctx.result("Lesson ID: " + ctx.pathParam("id"));
+        });*/
 
         app.start(7070);
     }
 }
+/*    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        // Позже мы разберем эти конструкции подробнее
+        var user = UserRepository.find(id) // Ищем пользователя в базе по id
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+    }
+}*/
